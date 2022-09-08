663db16ccde06f22c30d723f82d0d3c5b2d4b059
leandrosardi <leandro.sardi@expandedventure.com>
2022-09-08 12:42:39 -0300

    #2

--- mta-deployer.rb
+++ mta-deployer.rb
@@ -4,6 +4,7 @@
 # require all needed gems here.
 require "blackstack-nodes"
 require 'blackstack-deployer'
+require 'simple_cloud_logging'
 require 'net/http'
 require 'nokogiri'
 
@@ -19,42 +20,66 @@ module BlackStack
         
         # inherit BlackStack::Deployer::NodeModule, including features of deployer.
         module NodeModule
-#            attr_accessor :deployment_routine, :parameters
+            attr_accessor :domain, :addresses
     
             include BlackStack::Deployer::NodeModule
         
             def self.descriptor_errors(h)
-                errors = BlackStack::Deployer::NodeModule.descriptor_errors(h)
-        
+                # default deployment reoutine - required by BlackStack::Deployer::NodeModule
+                h[:deployment_routine] = 'mta-deployer-deploy' if h[:deployment_routine].nil?
+                # validations by BlackStack::Deployer::NodeModule
+                errors = BlackStack::Deployer::NodeModule.descriptor_errors(h)        
                 # validate: h must have :domain attribute and it must be a string
                 errors << "The parameter h does not have a key :domain" unless h.has_key?(:domain)
                 errors << "The parameter h[:domain] is not a string" unless h[:domain].is_a?(String)
-
-                # validate: h must have :addresses attribute and it must be an array
+                # validate: :domain is a valid domain name: sld.tld
+                errors << "The parameter h[:domain] is not a valid domain name" unless h[:domain].to_s.match(/^[a-z0-9\-\.]+\.[a-z]{2,8}$/i)
+                # validate: h must have :addresses attribute and it must be an array of strings
                 errors << "The parameter h does not have a key :addresses" unless h.has_key?(:addresses)
                 errors << "The parameter h[:addresses] is not an array" unless h[:addresses].is_a?(Array)
-
-                
-                
+                h[:addresses].each do |address|
+                    errors << "The parameter h[:addresses] contains a non-string element" unless address.is_a?(String)
+                end
                 # return list of errors
                 errors.uniq
             end
     
             def initialize(h, i_logger=nil)
+                # mappping attributes
                 self.parameters = h
                 errors = BlackStack::MtaDeployer::NodeModule.descriptor_errors(h)
                 raise "The node descriptor is not valid: #{errors.uniq.join(".\n")}" if errors.length > 0
                 super(h, i_logger)
+                self.domain = h[:domain]
+                self.addresses = h[:addresses]
                 self.deployment_routine = h[:deployment_routine]
             end # def self.create(h)
         
             def to_hash
                 h = super
-                h[:deployment_routine] = self.deployment_routine
+                h[:domain] = self.domain
+                h[:addresses] = self.addresses
                 h
-            end # def to_hash    
+            end # def to_hash
+            
+            # setup MX, SPF, DKIM and DMARC records for the domain
+            def setup_dns(host_name, record_type, address, ttl=3600)
+                sld = self.domain.split('.').first
+                tld = self.domain.split('.').last
+                url = "https://api.namecheap.com/xml.response?apiuser=#{namecheap_user}&apikey=#{namecheap_api_key}&username=#{namecheap_username}&Command=namecheap.domains.dns.setHosts&ClientIp=#{namecheap_client_ip}&SLD=#{sld}&TLD=#{tld}&HostName1=#{host_name}&RecordType1=#{record_type}&Address1=#{address}&TTL1=#{ttl}"
+            end
+
         end # module NodeModule
     
+        # TODO: declare these classes (stub and skeleton) using blackstack-rpc
+        #
+        # Stub Classes
+        # These classes represents a node, without using connection to the database.
+        # Use this class at the client side.
+        class Node
+            include BlackStack::MtaDeployer::NodeModule
+        end # class Node
+
         # validate the configuration descriptor
         def self.descriptor_errors(h)
             errors = []
@@ -70,6 +95,15 @@ module BlackStack
                 errors << "The parameter h does not have a key :namecheap_username or it is not a string" unless h.has_key?(:namecheap_username) && h[:namecheap_username].is_a?(String)
                 # validate: the parameter h has a key :namecheap_client_ip and it is a string
                 errors << "The parameter h does not have a key :namecheap_client_ip or it is not a string" unless h.has_key?(:namecheap_client_ip) && h[:namecheap_client_ip].is_a?(String)
+                # validate: if exists :nodes, it most be an array, and each element must be a valid descritor of BlackStack::MtaDeployer::NodeModule
+                if h.has_key?(:nodes)
+                    errors << "The parameter h[:nodes] is not an array" unless h[:nodes].is_a?(Array)
+                    if h[:nodes].is_a?(Array)
+                        h[:nodes].each do |node|
+                            errors += BlackStack::MtaDeployer::NodeModule.descriptor_errors(node)
+                        end
+                    end # if h[:nodes].is_a?(Array)
+                end
             end # if h.is_a?(Hash)
             # return
             errors
